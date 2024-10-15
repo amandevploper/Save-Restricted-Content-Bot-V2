b41b5ef36c8abc29672b37c5a3a2d3bd1b719068
amandevploper <rajeshgiri3824ptc@gmail.com>
2024-10-15 12:19:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "").split()))
-MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "")
-CHANNEL_ID = int(getenv("CHANNEL_ID", ""))
+API_ID = int(getenv("API_ID", "23401377"))
+API_HASH = getenv("API_HASH", "a253e1cdcb1bdbad27dc4f1fcefca125")
+BOT_TOKEN = getenv("BOT_TOKEN", "7606701563:AAFMSIS1sKFCp4jiDx4NtR3bcnGH1SwUmVs")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "7608856647").split()))
+MONGO_DB = getenv("MONGO_DB", "mongodb+srv://rajeshgiri3824ptc:amangiri1026gms@cluster0.aqxkd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+LOG_GROUP = getenv("LOG_GROUP", "1002326063269")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "amangiribot"))
